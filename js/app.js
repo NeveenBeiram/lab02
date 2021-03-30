477b7ec331f7c20aa6bae5ce5d1ffbfb206d923c
NeveenBeiram <neveenbeiram@yahoo.com>
2021-03-30 18:23:53 +0300

    done the first and second feature

--- js/app.js
+++ js/app.js
@@ -3,14 +3,49 @@ $( document ).ready( function(){
 
 
   let keywords = [];
+  let galary;
   $.ajax( './data/page-1.json' )
     .then( data => {
       data.forEach( dataVal => {
-        let galary = new Galary( dataVal );
+        galary = new Galary( dataVal );
         galary.renderTemplate();
       } );
-      $('#photo-template').first().remove();
+      // $('#photo-template').first().remove();
     } );
+
+  $('#first').on('click',function (){
+    keywords=[];
+    $('main').empty();
+    $('.keyword > option').not(':first').remove();
+    $.ajax( './data/page-1.json' )
+      .then( data => {
+        data.forEach( dataVal => {
+          // console.log(dataVal+'nnnn');
+          galary = new Galary( dataVal );
+          galary.renderTemplate();
+        } );
+      // $('#photo-template').first().remove();
+      } );
+    // galary.renderTemplate();
+  });
+
+
+  $('#second').on('click',function(){
+    keywords=[];
+    $('main').empty();
+    $('.keyword > option').not(':first').remove();
+
+    $.ajax('./data/page-2.json')
+      .then(data2 =>{
+        data2.forEach(dataVal2=>{
+          let galary2 = new Galary(dataVal2);
+          galary2.renderTemplate();
+        });
+      });
+  });
+
+
+
   function Galary ( obj ){
     this.title = obj.title;
     this.img = obj.image_url;
@@ -20,19 +55,30 @@ $( document ).ready( function(){
   }
   Galary.prototype.renderTemplate = function(){
 
-    let tempClone = $( '#photo-template' ).first().clone();
-    tempClone.addClass( this.keyword );
-    tempClone.find( 'img' ).attr( 'src',this.img );
-    tempClone.find( 'h2' ).text( this.title );
-    tempClone.find('p').text(this.description);
-    $( 'main' ).append( tempClone );
+    // let tempClone = $( '#photo-template' ).first().clone();
+    // tempClone.addClass( this.keyword );
+    // tempClone.find( 'img' ).attr( 'src',this.img );
+    // tempClone.find( 'h2' ).text( this.title );
+    // tempClone.find('p').text(this.description);
+    // $( 'main' ).append( tempClone );
+
+
+
+    let tTemplate=$('#photo-Mustache').html();
+    let renderTemplate=Mustache.render(tTemplate,this);
+    $('main').append(renderTemplate);
+
+    // $('#first').click(function(){
+
+    // });
+
     if ( !( keywords.includes( this.keyword ) ) ){
       keywords.push( this.keyword );
       let optionElement = $( '<option> </option>' ).attr( 'value',this.keyword ).text( this.keyword );
-      $( 'select' ).append( optionElement );
+      $( '.keyword' ).append( optionElement );
     }
   };
-  $( 'select' ).change( ( e ) => {
+  $( '.keyword' ).change( ( e ) => {
     $( 'div' ).hide();
     let targetValue = e.target.value;
     $( `.${targetValue}` ).show();
@@ -40,6 +86,22 @@ $( document ).ready( function(){
       $( 'div' ).show();
     }
   } );
+
+  $('#filter').change((e)=>{
+    let targetValue =e.target.value ;
+    if(targetValue==='title'){
+
+    }
+
+    else if(targetValue==='horns'){
+      
+    }
+    else if(targetValue==='sortBy'){
+
+    }
+  });
+
+
 } );
 
 
